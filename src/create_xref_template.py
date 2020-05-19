6ed123b588e79e265901b07aa37d68745ab76263
rctauber <rctauber@gmail.com>
2020-05-19 09:16:09 -0700

    Add IDs back to mapping templates

--- src/create_xref_template.py
+++ src/create_xref_template.py
@@ -17,41 +17,48 @@ def main():
     xref_template = args.xref_template
 
     # make a map of index label to ID
-    reader = csv.reader(mapping_index, delimiter='\t')
-    next(reader)
-    next(reader)
     label_to_id = {}
+    reader = csv.reader(mapping_index, delimiter='\t')
+    # Skip header and template rows
+    next(reader)
+    next(reader)
     for row in reader:
         if not row:
+            # Row is empty
             continue
+        # Get the GECKO CURIE
         gecko_id = row[0].strip()
         if gecko_id == '':
+            # No curie, skip row
             continue
-        alt_label = row[1].strip()
+        # Get the label
         gecko_label = row[2].strip()
         if gecko_label == '':
+            # No label, skip row
             continue
         if gecko_label in label_to_id:
-            print('WARNING: label "{0}" already exists with as {1}'.format(
+            print('ERROR: label "{0}" already exists with as {1}'.format(
                 gecko_label, label_to_id[gecko_label]))
             continue
-        if alt_label != '' and alt_label not in label_to_id:
-            label_to_id[alt_label] = gecko_id
+        # Add to map
         label_to_id[gecko_label] = gecko_id
     mapping_index.close()
 
+    # Create the Xref template headers
     writer = csv.writer(xref_template, delimiter='\t')
-    writer.writerow(['Label', 'Xref'])
-    writer.writerow(['LABEL', 'AI database_cross_reference SPLIT=|'])
+    writer.writerow(['ID', 'Xref', 'Xref Label'])
+    writer.writerow(['ID', 'AI database_cross_reference', '>A label'])
+
+    # Read in the mapping template to get Xrefs
     reader = csv.reader(mapping_template, delimiter='\t')
     next(reader)
     next(reader)
     for row in reader:
-        label = row[0].strip()
-        gecko_labels = row[2].strip()
+        curie = row[0].strip()
+        gecko_labels = row[3].strip()
         if gecko_labels == '':
             continue
-        iris = []
+        iris_labels = {}
         for gl in gecko_labels.split('|'):
             if gl == '':
                 continue
@@ -60,8 +67,10 @@ def main():
             if gl.strip() not in label_to_id:
                 print('ERROR: "{0}" not in index!'.format(gl))
                 continue
-            iris.append(label_to_id[gl.strip()].replace('GECKO:', 'http://example.com/GECKO_'))
-        writer.writerow([label, '|'.join(iris)])
+            iri = label_to_id[gl.strip()].replace('GECKO:', 'http://example.com/GECKO_')
+            iris_labels[iri] = gl.strip()
+        for i, l in iris_labels.items():
+            writer.writerow([curie, i, l])
 
 
 if __name__ == '__main__':
