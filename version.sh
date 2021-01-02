861049b3d0b91862668c122f3f6a66329b769cf2
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-01-02 18:11:57 +0000

    Update version.sh

--- version.sh
+++ version.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-# VERSION_IRI = $1
-VERSION_IRI="https://purl.ihccglobal.org/vz/releases/2020-12-01/vz.owl"
+VERSION_IRI=$1
+#VERSION_IRI="https://purl.ihccglobal.org/vz/releases/2020-12-01/vz.owl"
 DATE=$(echo "$VERSION_IRI" |grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}' )
 DATE=$DATE" 23:59:59"
 DATA_DICT=$(echo $VERSION_IRI | sed 's:.*/::')
 
-echo $DATE
+#echo $DATE
 
-echo $DATA_DICT
+#echo $DATA_DICT
 
 
 if git rev-parse --git-dir > /dev/null 2>&1; then
@@ -23,8 +23,8 @@ if git rev-parse --git-dir > /dev/null 2>&1; then
   #git checkout 'master@{'"$DATE"'}' --quiet
   COMMIT_ID2=$(git log -n1 --before "$DATE" --pretty=format:"%h")
   echo "https://raw.githubusercontent.com/IHCC-cohorts/data-harmonization/$COMMIT_ID2/data_dictionaries/$DATA_DICT"
-
   
 else
   echo "Not a git repo, aborting"
+  exit 1
 fi
