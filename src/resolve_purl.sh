9248d89ef6f5abd29a352bba7658a8b1cc20d0c8
James A. Overton <james@overton.ca>
2021-01-29 15:26:43 -0500

    Fix problems from server deployment

--- src/resolve_purl.sh
+++ src/resolve_purl.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 #
 # Given an URL path, return an HTTP 301 to GitHub or a 404.
-# Can also be called as a CGI script, which expects a PATH_INFO.
+# Can also be called as a CGI script, which expects a QUERY_STRING.
 
-PATH_INFO=${PATH_INFO=$1}
+PATH_INFO=${QUERY_STRING=$1}
 GITHUB="https://raw.githubusercontent.com/IHCC-cohorts/data-harmonization"
+DICTS="../data_dictionaries"
 LOCATION=""
 
 # Determine PURL type: latest or versioned.
-LATEST=$(expr "${PATH_INFO}" : '^/[[:alpha:]]\+.owl$')
-VERSIONED=$(expr "${PATH_INFO}" : '^/[[:alpha:]]\+/releases/[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+/[[:alpha:]]\+.owl$')
+LATEST=$(expr "${PATH_INFO}" : '^[[:alpha:]]\+.owl$')
+VERSIONED=$(expr "${PATH_INFO}" : '^[[:alpha:]]\+/releases/[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+/[[:alpha:]]\+.owl$')
 
 # echo ${LATEST} ${VERSIONED} ${PATH_INFO}
 
 # Get the version date for a commit and a file as an integer, e.g. 20210101
 date() {
-   git show $1:data_dictionaries/${2} \
+   git show "$1:${DICTS}/${2}" \
    | grep -m1 "owl:versionIRI rdf:resource=" \
    | sed 's/.*\([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\).*/\1\2\3/'
 }
 
 if [ ${LATEST} -gt 0 ]; then
-    FILE="$(echo ${PATH_INFO} | sed s:/::)"
-    if [ -f "data_dictionaries/${FILE}" ]; then
+    FILE="${PATH_INFO}"
+    if [ -f "${DICTS}/${FILE}" ]; then
         LOCATION="${GITHUB}/master/data_dictionaries/${FILE}"
     fi
 elif [ ${VERSIONED} -gt 0 ]; then
-    DATE=$(echo "${PATH_INFO}" | cut -d'/' -f4 | sed 's/-//g')
-    FILE=$(echo "${PATH_INFO}" | cut -d'/' -f5)
-    COMMIT_IDS=$(git log --follow -- "data_dictionaries/${FILE}" | grep commit | sed 's/^commit //')
+    DATE=$(echo "${PATH_INFO}" | cut -d'/' -f3 | sed 's/-//g')
+    FILE=$(echo "${PATH_INFO}" | cut -d'/' -f4)
+    COMMIT_IDS=$(git log --follow -- "${DICTS}/${FILE}" | grep commit | sed 's/^commit //')
     for COMMIT_ID in $COMMIT_IDS
         do
             DATE2="$(date ${COMMIT_ID} ${FILE})"
@@ -44,8 +45,12 @@ fi
 if [ "${LOCATION}" ]; then
     echo "Status: 301 Moved Permanently"
     echo "Location: ${LOCATION}"
+    echo "Content-Type: text/html"
+    echo ""
+    echo "${LOCATION}"
 else
     echo "Status: 404 Not Found"
+    echo "Content-Type: text/html"
+    echo ""
+    echo "Error 404: Not Found"
 fi
-
-echo "Content-Type: text/html"
--- src/test_resolve_purl.sh
+++ src/test_resolve_purl.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-echo "testing successful ones:"
-sh resolve_purl.sh /vz.owl
-sh resolve_purl.sh /vz/releases/2020-12-01/vz.owl
-sh resolve_purl.sh /vz/releases/2020-10-07/vz.owl
-sh resolve_purl.sh /ge/releases/2020-09-24/ge.owl
+echo "TEST 301"
+echo ""
+sh resolve_purl.sh vz.owl
+sh resolve_purl.sh vz/releases/2020-12-01/vz.owl
+sh resolve_purl.sh vz/releases/2020-10-07/vz.owl
+sh resolve_purl.sh ge/releases/2020-09-24/ge.owl
 
-echo "testing bad ones"
-sh resolve_purl.sh /vz/2020-12-01/vz.owl
-sh resolve_purl.sh /vz/releases/2020-12-02/vz.owl
-#sh resolve_purl.sh /vz/releases/2020-10-08/vz.owl
-#sh resolve_purl.sh /vz/releases/2028-12-02/vz.owl
-#sh resolve_purl.sh /vz/releases/2020-10-03/ge.owl
-#sh resolve_purl.sh /ge/releases/2020-09-27/ge.owl
+echo "TEST 404"
+echo ""
+sh resolve_purl.sh vz/2020-12-01/vz.owl
+sh resolve_purl.sh vz/releases/2020-12-02/vz.owl
+#sh resolve_purl.sh vz/releases/2020-10-08/vz.owl
+#sh resolve_purl.sh vz/releases/2028-12-02/vz.owl
+#sh resolve_purl.sh vz/releases/2020-10-03/ge.owl
+#sh resolve_purl.sh ge/releases/2020-09-27/ge.owl
